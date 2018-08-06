865ad0554ca869ec7eda0c798c4227284f2b7317
rochellesagh <rsrochelli@gmail.com>
2018-08-06 18:23:16 +0000

    automatically backed up by learn

--- prac.rb
+++ prac.rb
@@ -1,5 +1,5 @@
 first_name = "Rochelle"
 last_name = "Saghirnejad"
 age = 17
-#String Interpretation
-puts "My name is " + first_name + " " + last_name
+# String Interpretation
+puts "My first name is " #{first_name} and I am #{age.to_s}"
